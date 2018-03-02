2cc8dcb30a19098498b3ca03b80c83a1838cf5ec
sofiane <sofiane@skylights.aero>
2018-03-02 07:48:06 +0100

    created rgb random value function

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ var redValue = document.querySelector("#redValue");
 var blueValue = document.querySelector("#blueValue");
 var greenValue = document.querySelector("#greenValue");
 
-var body = document.getElementsByTagName("body")[0];
+var easyButton = document.querySelector(".easyModeButton");
+var hardButton = document.querySelector(".hardModeButton");
 
-body.style.background = "black";
-// console.log(body);
+var currentRgbValues = [0, 0, 0];
+
+
+
+function setHeaderBackgroundColor(p_color) {
+  let body = document.getElementsByTagName("body")[0];
+  body.style.background = p_color;
+}
+
+function getRandomColorValue() {
+  // generate a random int between 0 (include) and 256 (exclude)
+  return Math.floor(Math.random() * Math.floor(256));
+}
+
+function generateRandomRgb() {
+  currentRgbValues = [
+    getRandomColorValue(),
+    getRandomColorValue(),
+    getRandomColorValue()
+  ];
+}
+
+console.log(currentRgbValues);
+generateRandomRgb();
+console.log(currentRgbValues);
