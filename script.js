a568e924448b57c57b5e409f6e3cc3b2bfdb3eb1
sofiane <sofiane@skylights.aero>
2018-03-02 07:34:58 +0100

    Finished HTML and CSS design, let's do JS now :)

---
+++ script.js
@@ -0,0 +1,10 @@
+var easyMode = true;
+
+var redValue = document.querySelector("#redValue");
+var blueValue = document.querySelector("#blueValue");
+var greenValue = document.querySelector("#greenValue");
+
+var body = document.getElementsByTagName("body")[0];
+
+body.style.background = "black";
+// console.log(body);
